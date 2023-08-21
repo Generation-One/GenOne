1b5d95c057795393cc7f9d763cd4200967c365a5
Dmitry Kaznacheev <dikaznaceev@gmail.com>
2023-08-21 18:02:23 +1000

    update publish

--- build-packages.ps1
+++ build-packages.ps1
@@ -1,17 +1,29 @@
 $output = "./nupkgs"
 $version = "0.0.1"
 $publishConfig = "./nuget/publish-config.json"
+
 $json = Get-Content $publishConfig -Raw | ConvertFrom-Json
 
-Write-Host $json.packagesPaths
+Write-Host "Packages to publish"
+Write-Host "--------------------------------------------------------"
+Write-Host ($json.packagesPaths -join "`n")
 
 dotnet restore
 
 dotnet build --no-restore --configuration Release
 
-dotnet test --no-build --verbosity normal --configuration Release
-
-foreach ( $path in $json.packagesPaths ){
-    dotnet pack $path --output $output --no-build --configuration Release -p:PackageVersion=$version 
+if($LASTEXITCODE -ne 0)
+{
+    throw "dotnet build failed"
 }
 
+dotnet test --no-build --verbosity normal --configuration Release
+
+if($LASTEXITCODE -ne 0)
+{
+    throw "dotnet test failed"
+}
+
+foreach ( $path in $json.packagesPaths ){
+    $r = dotnet pack $path --output $output --no-build --configuration Release -p:PackageVersion=$version
+}
--- publish-nuget.ps1
+++ publish-nuget.ps1
@@ -1,7 +1,18 @@
 $artifacts = "./nupkgs"
 $NugetApiKey = ""
+$publishConfig = "./nuget/publish-config.json"
+
+$json = Get-Content $publishConfig -Raw | ConvertFrom-Json
 
 $filenames = (Get-ChildItem -Path $artifacts | Select Name).Name
+
+$count =  $json.packagesPaths.Count
+$actualCount = $filenames.Length
+
+if($count -ne $actualCount)
+{
+    throw "Packages count is $count but actual count is $actualCount"
+}
 
 foreach($name in $filenames){
     dotnet nuget push ./nupkgs/$name --source https://api.nuget.org/v3/index.json --api-key $NugetApiKey
